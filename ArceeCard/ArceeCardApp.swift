58cb8506bdb9797629cffb264b9be58cfbe28282
arshaansayed <arshaan.h.sayed@gmail.com>
2022-06-24 19:08:23 -0400

    Initial Commit

---
+++ ArceeCard/ArceeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArceeCardApp.swift
+//  ArceeCard
+//
+//  Created by Arshaan Sayed on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ArceeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArceeCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArceeCard
+//
+//  Created by Arshaan Sayed on 6/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
